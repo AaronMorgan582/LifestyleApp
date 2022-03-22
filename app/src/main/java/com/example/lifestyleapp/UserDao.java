cea7298cfa7cf87699f97d251e63bc9a2735eb58
AaronMorgan582 <aaron.st.morgan@gmail.com>
2022-03-21 21:29:02 -0600

    Created UserDao and UserTable

---
+++ app/src/main/java/com/example/lifestyleapp/UserDao.java
@@ -0,0 +1,20 @@
+package com.example.lifestyleapp;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(UserTable userTable);
+
+    @Query("SELECT * from user_table ORDER BY first_name ASC")
+    LiveData<List<UserTable>> getAll();
+
+    //TODO: Will likely need to make a query for a specific user.
+}
